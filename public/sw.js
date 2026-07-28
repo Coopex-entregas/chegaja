b180284604ce7f41444c01fb0d50b61d6fd665b0
Coopex-entregas <coopexentregas.rn@gmail.com>
2026-07-28 16:11:11 -0300

    Atualizar cache para aplicativo único 14.19.0

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,14 @@
-const CACHE='chegaja-static-14-18-4';
+const CACHE='chegaja-static-14-19-0';
 const CORE=[
   '/','/index.html','/manifest.webmanifest',
   '/icons/icon-official.png','/icons/logo-official.png',
   '/vendor/leaflet/leaflet.css','/vendor/leaflet/leaflet.js','/vendor/qrcode.js',
   '/chegaja-final.css?v=14.15.9','/chegaja-v144.css?v=14.15.9','/chegaja-v145.css?v=14.15.9',
   '/chegaja-v148.css?v=14.15.9','/chegaja-v149.css?v=14.15.9','/chegaja-mobile-app.css?v=14.16.0',
-  '/chegaja-v180.css?v=14.18.1','/chegaja-v182.css?v=14.18.2','/chegaja-v183.css?v=14.18.4','/chegaja-v184.css?v=14.18.4',
+  '/chegaja-v190.css?v=14.19.0',
   '/app.js?v=14.15.9','/chegaja-maps-google.js?v=14.18.1','/chegaja-final.js?v=14.15.9',
   '/chegaja-v144.js?v=14.15.9','/chegaja-v145.js?v=14.15.9','/chegaja-v148.js?v=14.15.9',
-  '/chegaja-v149.js?v=14.15.9','/chegaja-v180.js?v=14.18.1','/chegaja-v181.js?v=14.18.1',
-  '/chegaja-v182.js?v=14.18.2','/chegaja-v183.js?v=14.18.4'
+  '/chegaja-v149.js?v=14.15.9','/chegaja-v190.js?v=14.19.0'
 ];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).catch(()=>{}))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))]))});
