84f5d3233a7af76ac9bca4738e3015d19c2cb893
Coopex-entregas <coopexentregas.rn@gmail.com>
2026-07-28 13:31:30 -0300

    Atualizar cache para navegação 14.18.2

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,15 @@
-const CACHE='chegaja-static-14-17-4';
+const CACHE='chegaja-static-14-18-2';
 const CORE=[
   '/','/index.html','/manifest.webmanifest',
   '/icons/icon-official.png','/icons/logo-official.png',
   '/vendor/leaflet/leaflet.css','/vendor/leaflet/leaflet.js','/vendor/qrcode.js',
   '/chegaja-final.css?v=14.15.9','/chegaja-v144.css?v=14.15.9','/chegaja-v145.css?v=14.15.9',
   '/chegaja-v148.css?v=14.15.9','/chegaja-v149.css?v=14.15.9','/chegaja-mobile-app.css?v=14.16.0',
-  '/chegaja-realtime-safe.css?v=14.17.0','/chegaja-v172.css?v=14.17.4',
-  '/app.js?v=14.15.9','/chegaja-final.js?v=14.15.9','/chegaja-v144.js?v=14.15.9',
-  '/chegaja-v145.js?v=14.15.9','/chegaja-v148.js?v=14.15.9','/chegaja-v149.js?v=14.15.9',
-  '/chegaja-customer-loader.js?v=14.17.4','/chegaja-v172.js?v=14.17.4'
+  '/chegaja-v180.css?v=14.18.1','/chegaja-v182.css?v=14.18.2',
+  '/app.js?v=14.15.9','/chegaja-maps-google.js?v=14.18.1','/chegaja-final.js?v=14.15.9',
+  '/chegaja-v144.js?v=14.15.9','/chegaja-v145.js?v=14.15.9','/chegaja-v148.js?v=14.15.9',
+  '/chegaja-v149.js?v=14.15.9','/chegaja-v180.js?v=14.18.1','/chegaja-v181.js?v=14.18.1',
+  '/chegaja-v182.js?v=14.18.2'
 ];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).catch(()=>{}))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))]))});
